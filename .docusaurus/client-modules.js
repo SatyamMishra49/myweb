ed65794eed6e447aa1e29650b3c5e683be072678
SatyamMishra49 <skm28112001@gmail.com>
2025-04-09 22:40:32 +0530

    deployed

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\iamsa\\Desktop\\myweb\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\iamsa\\Desktop\\myweb\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\iamsa\\Desktop\\myweb\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\iamsa\\Desktop\\myweb\\src\\css\\custom.css"),
+];
